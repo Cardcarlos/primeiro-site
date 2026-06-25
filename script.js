4e799d8497ef73bd7c24fdacd82597b81bff9e42
Cardcarlos <carlos.silva.oliveira09@escola.pr.gov.br>
2026-06-25 09:58:01 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const curtidas = document.getElementById("curtidas") // Pega o botão de curtida pelo
+curtidas.addEventListener("click", aumetarCurtidas)
+
+//função de aumetar curtidas 
+function aumetarCurtidas(){
+    let quantidade = document.querySelector("span")
+    quantitade.textContent++;
+}
